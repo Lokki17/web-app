45897a2f1aca55627555a27e5de1fe9b718927d3
Lokki17 <df1985@mail.ru>
2017-07-27 08:29:02 +0300

    1

---
+++ src/app/factory/coffee.factory.ts
@@ -0,0 +1,8 @@
+export class CoffeeKind {
+
+    constructor(private id: number,
+                private name: string,
+                private description: string,
+                private price: number) {
+    }
+}
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,14 @@
+
+
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+
+@Injectable()
+export class HttpService {
+    constructor(private http: Http) {
+    }
+
+    getData() {
+        return this.http.get('http://localhost:8080/kind');
+    }
+}
