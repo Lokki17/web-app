81e3f15621de9404760a21b452c991b5fe96bc8d
Lokki17 <df1985@mail.ru>
2017-07-28 08:33:21 +0300

    3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,48 @@ import {CoffeeKind} from './factory/coffee.factory';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
+    template: `
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Наименование</th>
+                <th>Цена</th>
+                <th>Количество</th>
+            </tr>
+            <tr *ngFor="let kind of coffeeKinds">
+                <td>{{kind.id}}</td>
+                <td>{{kind.name}}</td>
+                <td>{{kind.price}}</td>
+                <td><input type="number" min="0" required>
+                    <div></div>
+                </td>
+            </tr>
+            <div>
+                <p>Ваше имя: {{name}}</p>
+            </div>
+            <div [hidden]="address">
+                <p>Ваш адрес: {{address}}</p>
+            </div>
+        </table>
+        <button [disabled]="hidden">Сделать заказ</button>
+        <button>Введите свое имя и адрес</button>
+        `,
     styleUrls: ['./app.component.css'],
     providers: [HttpService]
 })
 export class AppComponent implements OnInit {
+    hidden = true;
     title = 'app';
+    name = 'Вася';
+    address;
     coffeeKinds: CoffeeKind[] = [];
 
     constructor(private httpService: HttpService) {
+        console.log('Test');
     }
 
     ngOnInit(): void {
-        this.httpService.getCoffeeKinds().subscribe((data) => this.coffeeKinds);
-        throw new Error('Method not implemented.');
+        this.httpService.getCoffeeKinds().subscribe((data) => this.coffeeKinds = data);
+        console.log(this.coffeeKinds);
     }
-
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,14 +2,38 @@
 
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import {CoffeeKind} from '../factory/coffee.factory';
 
 @Injectable()
 export class HttpService {
+    public error: any;
     constructor(private http: Http) {
     }
-
-    getCoffeeKinds() {
-        // return this.http.get('http://localhost:8080/kind');
-        return this.http.get('coffee.json');
+    static handleError(error: any) {
+        console.error(error);
+        return Observable.throw(error);
     }
+
+    public getCoffeeKinds() {
+        console.log('Test');
+        return this.http.get('http://localhost:8080/kind')
+            .map(response => {
+                console.log('response');
+                return <any[]>response.json();
+            })
+            .catch(HttpService.handleError);
+    }
+
+    private extractData(res: Response | any) {
+        console.log('Test');
+        const body = res.json();
+        return body || {};
+    }
+
+
 }
