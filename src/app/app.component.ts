09a655f59ba1dae233fee6551ef0c0d26de27b39
Lokki17 <df1985@mail.ru>
2017-07-29 17:02:55 +0300

    8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,78 +6,7 @@ import {AppSettings} from './app.settings';
 
 @Component({
     selector: 'app-root',
-    template: `
-        <div>
-            <p>Поиск по названию и описанию</p>
-            <input type="text" [(ngModel)]="request"/>
-            <button (click)="refresh(request, '?key=')">Поиск</button>
-        </div>
-        <br>
-        <button (click)="refresh()">Получить список сортов кофе</button>
-        <div [hidden]="!(coffeeKinds.length > 0)">
-            <table>
-                <tr>
-                    <th>Id</th>
-                    <th>Наименование</th>
-                    <th>Цена</th>
-                    <th>Количество</th>
-                </tr>
-                <tr *ngFor="let kind of coffeeKinds">
-                    <td>{{kind.id}}</td>
-                    <td>{{kind.name}}</td>
-                    <td>{{kind.price}}</td>
-                    <td><input type="number" required [(ngModel)]="kind.count"></td>
-                    <div [hidden]="checkInput(kind)">
-                        <td>Введено некорректное значение</td>
-                    </div>
-                </tr>
-            </table>
-        </div>
-        <div [hidden]="!userName">
-            <p>Ваше имя: {{userName}}</p>
-        </div>
-        <div [hidden]="!address">
-            <p>Ваш адрес: {{address}}</p>
-        </div>
-        <div [hidden]="!address || !userName">
-            <button (click)="createOrder()">Сделать заказ</button>
-        </div>
-        <button (click)="openModal()">Введите свое имя и адрес</button>
-        <div bsModal #modalWindow="bs-modal" class="modal fade" (onHidden)="closeModal()">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title pull-left">Форма ввода данных клиента</h4>
-                        <button type="button" class="close pull-right" (click)="closeModal()" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div [ngSwitch]="modalType">
-                            <ng-template [ngSwitchCase]='mAddress'>
-                                <p>Введите свое имя</p>
-                                <input type="text" [(ngModel)]="userName"/>
-                                <div [hidden]="checkString(userName)">
-                                    <td>Введено некорректное значение</td>
-                                </div>
-                                <p>Введите свой адрес</p>
-                                <input type="text" [(ngModel)]="address"/>
-                                <div [hidden]="checkString(address)">
-                                    <td>Введено некорректное значение</td>
-                                </div>
-                            </ng-template>
-                            <ng-template [ngSwitchCase]='mError'>
-                                <p>Произошла ошибка. Проверьте правильность введенных данных и повторите попытку</p>
-                            </ng-template>
-                            <ng-template [ngSwitchCase]='mOrder'>
-                                <p>Стоимость вашего заказа равна {{order.price}}</p>
-                            </ng-template>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `,
+    templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     providers: [HttpService, ModalDirective]
 })
